16a7c9172fcb837bece65f35e7168b24606a4c13
yuanqiang <m13072866230_1@163.com>
2018-12-25 13:21:33 +0800

    init gshop

---
+++ src/main.js
@@ -0,0 +1,14 @@
+/**
+ * 入口js
+ */
+
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+// eslint-disable-next-line no-new
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+})
---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+/**
+ * 引入路由对象模块
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../pages/Login/Login.vue'
+import MSite from '../pages/MSite/MSite.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Search from '../pages/Search/Search.vue'
+//声明使用插件
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  //所有路由
+  routes: [
+      {
+        path: '/msite',
+        component: MSite,
+        meta:{
+          showFooter: true
+        }
+      },
+      {
+        path: '/search',
+        component: Search,
+        meta:{
+          showFooter: true
+        }
+      },
+      {
+        path: '/order',
+        component: Order,
+        meta:{
+          showFooter: true
+        }
+      },
+      {
+        path: '/profile',
+        component: Profile,
+        meta:{
+          showFooter: true
+        }
+      },
+      {
+        path: '/',
+        redirect: '/msite'
+      },
+      {
+        path: '/login',
+        component: Login
+      },
+  ]
+})
